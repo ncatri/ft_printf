63fcdb316f29754e798d123049515c05c1a85dc1
Nicolas Catriens <ncatrien@student.42lyon.fr>
2021-01-19 12:29:32 +0100

    %d is all good

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "include/ft_printf.h"
+#include <limits.h>
 
-#define TEST "|%3.5d|",-1 
+#define TEST "|%0*d|", 10, INT_MIN 
 
 int main(void)
 {
--- srcs/print_int.c
+++ srcs/print_int.c
@@ -6,7 +6,7 @@
 /*   By: ncatrien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 10:03:52 by ncatrien          #+#    #+#             */
-/*   Updated: 2021/01/19 08:26:50 by ncatrien         ###   ########lyon.fr   */
+/*   Updated: 2021/01/19 12:22:13 by ncatrien         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 void	print_int(t_format *f, va_list ap)
 {
+	char	*val0;
 	char	*val;
 	int		len;
 	int		d;
 
 	d = va_arg(ap, int);
-	if (d < 0)
+	val0 = ft_itoa_base(d, "0123456789");
+	if (val0[0] == '-')
 	{
-		d = -d;
 		f->negative = 1;
+		val = val0 + 1;
 	}
-	val = ft_itoa_base(d, "0123456789");
+	else
+		val = val0;
 	if (d == 0 && f->point)
 		val = "";
 	len = ft_strlen(val);
@@ -32,6 +35,7 @@ void	print_int(t_format *f, va_list ap)
 		left_justify(f, val, (int)len);
 	else
 		right_justify(f, val, (int)len);
+	free(val0);
 }
 
 void	left_justify(t_format *f, char *val, int len)
@@ -39,7 +43,7 @@ void	left_justify(t_format *f, char *val, int len)
 	int pad_prec;
 	int pad_width;
 
-	pad_prec = f->precision - len; 
+	pad_prec = f->precision - len;
 	if (!f->point || pad_prec < 0)
 		pad_prec = 0;
 	if (f->negative)
@@ -55,13 +59,22 @@ void	right_justify(t_format *f, char *val, int len)
 	int pad_prec;
 	int pad_width;
 
-	pad_prec = f->precision - len; 
+	pad_prec = f->precision - len;
 	if (!f->point || pad_prec < 0)
 		pad_prec = 0;
 	pad_width = f->width - len - pad_prec - f->negative;
-	f->nprinted += padding(' ', pad_width);
-	if (f->negative)
-		f->nprinted += write(1, "-", 1);
+	if (!f->zero || (f->zero && f->point))
+	{
+		f->nprinted += padding(' ', pad_width);
+		if (f->negative)
+			f->nprinted += write(1, "-", 1);
+	}
+	else
+	{
+		if (f->negative)
+			f->nprinted += write(1, "-", 1);
+		f->nprinted += padding('0', pad_width);
+	}
 	f->nprinted += padding('0', pad_prec);
 	f->nprinted += putstr_n(val, len);
 }
